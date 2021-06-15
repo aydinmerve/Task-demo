cfbb2727c70c55bdd7d04cf6edecf8f141268ef5
aydinmerve <merve9328@gmail.com>
2021-06-15 20:43:57 +0300

    input and file component created

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!--<input type="text" v-model="title" /> -->
+    <!--<input type="text" :value="title" @input="title=$event.target.value" />-->
+    <editor v-model="content"></editor>
+    <file></file>
+
   </div>
 </template>
 
 <script>
+import Editor from "./components/Editor";
+import File from "./components/file.vue";
 export default {
-  name: 'App'
+
+  name: 'App',
+  components: {
+    Editor,
+    File,
+  },
+  data(){
+    return{
+      title:"",
+      content:""
+    }
+  }
 }
 </script>
 
---
+++ demo/src/components/Editor.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+    <!--<input type="text" value="value" @input="updateValue($event.target.value)" /> -->
+     <form @submit.prevent="addInfo">
+         <input type="text" placeholder="add user info" v-model="usersInfo" />
+     </form>
+     <ul>
+         <li v-for="(data,index) in info" :key="index">{{index}}.{{data.usersInfo}}
+         </li>
+     </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"info",
+    data(){
+        return{
+        usersInfo:"",
+        info:[
+        
+      ]}
+      
+    },
+    props: ['value'],
+
+    methods:{
+        updateValue(value){
+            this.$emit('input', value);
+        },
+     addInfo(){
+         this.info.push({usersInfo:this.usersInfo});
+         this.usersİnfo='';
+
+     }
+    }
+}
+</script>
